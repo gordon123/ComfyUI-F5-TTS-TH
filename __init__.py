171149f2629b58983dc0ddcc20d0a29c65457e31
gordon123 <thegto_2001@yahoo.com>
2025-04-24 21:34:24 +0200

    init

--- __init__.py
+++ __init__.py
@@ -1,14 +1,18 @@
 # __init__.py
-from .F5TTS import F5TTSAudioInputs  # ลบ F5TTSThai ออก
+# รวม node classes ทั้งหมดไว้ใน package นี้
+
+from .F5TTS_Advance import F5TTS_Advance
+from .F5TTS import F5TTSAudioInputs
 from .save_audio_text_node import SaveAudioAndText
 
 NODE_CLASS_MAPPINGS = {
-    "F5TTSAudioInputs":   F5TTSAudioInputs,
-    "SaveAudioAndText":  SaveAudioAndText,
+    "F5TTS_Advance":     F5TTS_Advance,
+    "F5TTSAudioInputs":  F5TTSAudioInputs,
+    "SaveAudioAndText":   SaveAudioAndText,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "F5TTSAudioInputs":   "🇹🇭 F5‑TTS‑TH รับข้อมูลจาก Inputs 🇹🇭",
-    "SaveAudioAndText": "💾 🇹🇭 Save Audio & Text TH",
+    "F5TTS_Advance":     "🎤 F5‑TTS‑Advance TH",
+    "F5TTSAudioInputs":  "🇹🇭 F5‑TTS‑TH รับข้อมูลจาก Inputs 🇹🇭",
+    "SaveAudioAndText":  "💾 🇹🇭 Save Audio & Text TH",
 }
-
