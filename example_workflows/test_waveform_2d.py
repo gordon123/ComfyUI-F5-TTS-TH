b3054bee852fc9013bcfb5f2a3a7b81778fcef9f
gordon123 <thegto_2001@yahoo.com>
2025-04-29 22:58:16 +0200

    add sample muti speakers workflow

--- example_workflows/test_waveform_2d.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import torchaudio
-
-# โหลดไฟล์เสียงที่ต้องการทดสอบ
-waveform, sr = torchaudio.load("testTHmono.wav")
-
-# แสดงรูปร่างของ waveform
-print("✅ waveform shape:", waveform.shape)
